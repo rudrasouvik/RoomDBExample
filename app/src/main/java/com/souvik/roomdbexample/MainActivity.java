e264f0e1dc8bfa5e00566c50e210d5e51330403d
Souvik Rudra <svk.rudra@gmail.com>
2019-09-19 00:24:10 +0530

    Initial commit

--- app/src/main/java/com/souvik/roomdbexample/MainActivity.java
+++ app/src/main/java/com/souvik/roomdbexample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.souvik.roomdbexample;
 
+import android.arch.persistence.room.Room;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import com.souvik.roomdbexample.fragments.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
     public static FragmentManager fragmentManager;
+    public static MyAppDatabase myAppDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         fragmentManager = getSupportFragmentManager();
+        myAppDatabase = Room.databaseBuilder(getApplicationContext(), MyAppDatabase.class, "userdb").build();
         if(findViewById(R.id.fragment_container) != null){
             if(savedInstanceState != null){
                 return;
--- app/src/main/java/com/souvik/roomdbexample/fragments/AddUserFragment.java
+++ app/src/main/java/com/souvik/roomdbexample/fragments/AddUserFragment.java
@@ -6,14 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.souvik.roomdbexample.MainActivity;
 import com.souvik.roomdbexample.R;
+import com.souvik.roomdbexample.User;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AddUserFragment extends Fragment {
 
+    private EditText UserId, UserName, UserEmail;
+    private Button BnSave;
 
     public AddUserFragment() {
         // Required empty public constructor
@@ -24,7 +30,34 @@ public class AddUserFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_user, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_user, container, false);
+        UserId = view.findViewById(R.id.txt_user_id);
+        UserName = view.findViewById(R.id.txt_name);
+        UserEmail = view.findViewById(R.id.txt_email);
+        BnSave = view.findViewById(R.id.bn_save_user);
+
+        BnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int userid = Integer.parseInt(UserId.getText().toString());
+                String username = UserName.getText().toString();
+                String useremail = UserEmail.getText().toString();
+
+                User user = new User();
+                user.setId(userid);
+                user.setName(username);
+                user.setEmail(useremail);
+                MainActivity.myAppDatabase.myDao().addUser(user);
+
+                UserId.setText("");
+                UserName.setText("");
+                UserEmail.setText("");
+
+            }
+        });
+
+        return view;
     }
 
 }
