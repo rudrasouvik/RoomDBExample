c8a6c8ce2f29666e28a9782dd78a44bec80cc8e4
Souvik Rudra <svk.rudra@gmail.com>
2019-09-18 23:59:05 +0530

    Initial commit

--- app/src/main/java/com/souvik/roomdbexample/MainActivity.java
+++ app/src/main/java/com/souvik/roomdbexample/MainActivity.java
@@ -1,13 +1,25 @@
 package com.souvik.roomdbexample;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.souvik.roomdbexample.fragments.HomeFragment;
+
 public class MainActivity extends AppCompatActivity {
+    public static FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+        if(findViewById(R.id.fragment_container) != null){
+            if(savedInstanceState != null){
+                return;
+            }
+            fragmentManager.beginTransaction().add(R.id.fragment_container, new HomeFragment()).commit();
+        }
     }
 }
